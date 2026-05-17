5adcb2ef865ac438399f72470a30fd2584f5408b
Santosh <spant671@gmail.com>
2026-05-17 16:02:52 +0530

    Chapter 1: Add Problem 3 - list directory contents using os module; begin Chapter 2 with variables and data types

---
+++ Chapter 2/02_datatypes.py
@@ -0,0 +1,14 @@
+a = 1 
+# a is an integer
+
+b = 5.22
+#b is a floating point number
+
+c = "Alice"
+# c is a string
+
+d = False
+# d is a boolean value (True or False)
+
+e = None
+# e is a special value that represents "no value" or "null"
