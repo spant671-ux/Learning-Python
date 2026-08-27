348818d69502d7e7667bf6e09d6ab412c1be0b00
Santosh <spant671@gmail.com>
2026-08-27 18:16:39 +0530

    logical operators

--- 03_basics/02_logical_operators.py
+++ 03_basics/02_logical_operators.py
@@ -5,3 +5,17 @@ if temp > 35 or temp < 0 or is_raining:
     print("The outdoor event is cancelled.")
 else:
     print("The outdoor event is still scheduled.")
+
+temp2 = 30
+is_sunny = True
+
+if temp2 >= 28 and is_sunny:
+    print("It is hot outside 🥵")
+    print("It is SUNNY 🔆")
+elif temp2 <= 0 and is_sunny:
+    print("It is cold outside 🥶")
+    print("It is SUNNY 🔆")
+elif 28 > temp > 0 and is_sunny:
+    print("It is warm outside 😊")
+    print("It is SUNNY 🔆")
+    
